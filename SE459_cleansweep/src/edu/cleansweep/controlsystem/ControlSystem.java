548003e94f331ed2b6e0366c85a2dedad7f0f9a3
Patrick Mathieu <swapinfutures@gmail.com>
2015-10-24 12:34:14 -0500

    Adding Logical checks to the movement of the Control System

--- SE459_cleansweep/src/edu/cleansweep/controlsystem/ControlSystem.java
+++ SE459_cleansweep/src/edu/cleansweep/controlsystem/ControlSystem.java
@@ -16,55 +16,56 @@ public class ControlSystem {
 		floorNavProxy = new FloorNavigationProxy(f);
 		currentLocation = floorNavProxy.getStaringLocation();
 	}
+
 	
 	public void move()
 	{
-		System.out.println(currentLocation.toString());
-		
-		int i = 0;
-		
-		while(i < 100)
+		int random = (int)(Math.random() * (4 - 0) + 1);
+		switch(random)
 		{
-			int random = (int)(Math.random() * (4 - 0) + 1);
-			
-			switch(random)
-			{
-				case 1:
-					if(floorNavProxy.canMove(currentLocation, Direction.NORTH)){
-						currentLocation = floorNavProxy.move(currentLocation, Direction.NORTH);
-						System.out.println("Moving North");
-					}
-				break;
-				
-				case 2:
-					if(floorNavProxy.canMove(currentLocation, Direction.SOUTH)){
-						currentLocation = floorNavProxy.move(currentLocation, Direction.SOUTH);
-						System.out.println("Moving South");
-					}
-				break;
-				
-				case 3:
-					if (floorNavProxy.canMove(currentLocation, Direction.WEST)){
-						currentLocation = floorNavProxy.move(currentLocation, Direction.WEST);
-						System.out.println("Moving West");
-					}
-				break;
-				
-				case 4:
-					 if (floorNavProxy.canMove(currentLocation, Direction.EAST)){
-						 currentLocation = floorNavProxy.move(currentLocation, Direction.EAST);
-						 System.out.println("Moving East");
+			case 1:
+				if(floorNavProxy.canMove(currentLocation, Direction.NORTH)){
+					currentLocation = floorNavProxy.move(currentLocation, Direction.NORTH);
+					System.out.println("Moving North");
+					f.displayLocationOnFloorInConsole(currentLocation);
 				}
-				break;
+			break;
+			
+			case 2:
+				if(floorNavProxy.canMove(currentLocation, Direction.SOUTH)){
+					currentLocation = floorNavProxy.move(currentLocation, Direction.SOUTH);
+					System.out.println("Moving South");
+					f.displayLocationOnFloorInConsole(currentLocation);
+				}
+			break;
+			
+			case 3:
+				if (floorNavProxy.canMove(currentLocation, Direction.WEST)){
+					currentLocation = floorNavProxy.move(currentLocation, Direction.WEST);
+					System.out.println("Moving West");
+					f.displayLocationOnFloorInConsole(currentLocation);
+				}
+			break;
+			
+			case 4:
+				 if (floorNavProxy.canMove(currentLocation, Direction.EAST)){
+					 currentLocation = floorNavProxy.move(currentLocation, Direction.EAST);
+					 System.out.println("Moving East");
+					 f.displayLocationOnFloorInConsole(currentLocation);
 			}
-			i++;
+			break;
 		}
 	}
 	
 	public static void main(String [] args)
 	{
 		ControlSystem cs = new ControlSystem();
-		cs.move();
+		
+		int i = 0;
+		while(i < 10)
+		{
+			cs.move();
+			i++;
+		}
 	}
-
 }
