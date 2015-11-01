3965702ffc9e3c631dee487a2a72e176003ee257
Patrick Mathieu <swapinfutures@gmail.com>
2015-11-01 09:28:49 -0600

    Removing White Space

--- SE459_cleansweep/src/edu/cleansweep/controlsystem/FloorCleaners.java
+++ SE459_cleansweep/src/edu/cleansweep/controlsystem/FloorCleaners.java
@@ -1,5 +1,16 @@
 package edu.cleansweep.controlsystem;
 
-public class FloorCleaners {
+import edu.cleansweep.floor.FloorType;
 
+public class FloorCleaners 
+{
+	private FloorType _floorType = FloorType.BAREFLOOR;
+
+	public FloorType get_floorType() {
+		return _floorType;
+	}
+
+	public void set_floorType(FloorType _floorType) {
+		this._floorType = _floorType;
+	}
 }
