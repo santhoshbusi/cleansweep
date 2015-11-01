d637d419024e0495cf16f9f16b67f5be98b4e9e3
Patrick Mathieu <swapinfutures@gmail.com>
2015-11-01 09:26:41 -0600

    Adding Vacuum Classes to project

---
+++ SE459_cleansweep/src/edu/cleansweep/controlsystem/Vacuum.java
@@ -0,0 +1,34 @@
+package edu.cleansweep.controlsystem;
+
+
+import edu.cleansweep.floor.FloorNavigationProxy;
+import edu.cleansweep.floor.FloorType;
+import edu.cleansweep.floor.Location;
+
+public class Vacuum 
+{
+	private FloorType _floorType;
+	private FloorCleaners _floorCleaners;
+	public void doClean(Location location, FloorNavigationProxy floorProxy)
+	{
+		 //clean the location and move to new location
+		_floorType = floorProxy.getFloorType(location);
+		if(_floorType.equals(FloorType.BAREFLOOR))
+		{
+			_floorCleaners.set_floorType(_floorType);
+			floorProxy.clean(location);
+		}
+		if(_floorType.equals(FloorType.LOWPILECARPET))
+		{
+			_floorCleaners.set_floorType(_floorType);
+			floorProxy.clean(location);
+		}
+		if(_floorType.equals(FloorType.HIGHPILECARPET))
+		{
+			_floorCleaners.set_floorType(_floorType);
+			floorProxy.clean(location);
+		}
+		
+		
+	 }
+}
